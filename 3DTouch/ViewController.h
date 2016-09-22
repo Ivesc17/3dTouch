71706664c4ab54f7b703d1f3e8de175d14b30e09
ivesc17 <ivesc17@gmail.com>
2016-09-22 11:26:07 +0800

    init 3d touch project

---
+++ 3DTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTouch
+//
+//  Created by IvesChan on 2016/9/22.
+//  Copyright © 2016年 IvesChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3DTouch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  3DTouch
+//
+//  Created by IvesChan on 2016/9/22.
+//  Copyright © 2016年 IvesChan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
